35b5028cc2021e2fc0843b5a342f70033f48e2ba
Scott Noyes <snoyes@gmail.com>
2022-02-18 22:01:10 -0600

    refer to services rather than components

--- main.py
+++ main.py
@@ -19,18 +19,18 @@ if not os.path.exists('cache'):
 pygame.mouse.set_visible(config('SHOW_MOUSE', default=False, cast=bool))
 
 UPDATE_EVENTS = {}
-for component, update_frequency in update.list_active_components():
-    if (callback := update.get_update_callback(component)):
-        logging.info(f'Found callback for {component}')
+for service, update_frequency in update.list_active_services():
+    if (callback := update.get_update_callback(service)):
+        logging.info(f'Found callback for {service}')
         event_type = pygame.event.custom_type()
         UPDATE_EVENTS[event_type] = callback
         if update_frequency:
             pygame.time.set_timer(event_type, 1000 * update_frequency)
         if config('UPDATE_ON_STARTUP', default=False, cast=bool):
-            logging.info(f'Updating {component} data')
+            logging.info(f'Updating {service} data')
             callback()
     else:
-        logging.warning(f'No update callback found for {component}')
+        logging.warning(f'No update callback found for {service}')
 
 while True:
 
--- update.py
+++ update.py
@@ -1,26 +1,26 @@
 import sys, json
 from importlib import import_module
 
-def list_active_components():
+def list_active_services():
     with open('services.json') as json_file:
         grid_data = json.load(json_file)
     services = [(row.get('service'), row.get('update_interval_seconds', 0)) for row in grid_data['services'] if row.get('service')]
     return services
 
-#TODO: refactor this and the similar code in ui.py to a generic get_component_callback()
-def get_update_callback(component_name):
-    module = import_module('services.' + component_name)
+#TODO: refactor this and the similar code in ui.py to a generic get_callback()
+def get_update_callback(service_name):
+    module = import_module('services.' + service_name)
     return getattr(module, 'update', None)
 
 if __name__ == "__main__":
-    components = list_active_components()
-    for component, update_interval in components:
-        callback = get_update_callback(component)
+    services = list_active_services()
+    for service, update_interval in services:
+        callback = get_update_callback(service)
         if callback:
             if update_interval:
-                print(f'{component} will update every {update_interval} seconds.')
+                print(f'{service} will update every {update_interval} seconds.')
             else:
-                print(f'{component} has an update callback, but is not set to update at any interval.')
+                print(f'{service} has an update callback, but is not set to update at any interval.')
 
         else:
-            print(f'{component} has no update callback available.')
+            print(f'{service} has no update callback available.')
