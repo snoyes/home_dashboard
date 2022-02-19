b2a2296d72ea8107e3efa02a21109838bbbf83c2
Scott Noyes <snoyes@gmail.com>
2022-02-18 21:58:46 -0600

    remove duplicate methods

--- components/is_it_friday.py
+++ components/is_it_friday.py
@@ -21,23 +21,3 @@ def draw(screen, rect):
     text_rect = text.get_rect()
     text_rect.midtop = (center[0], content_y)
     screen.blit(text, text_rect)
-
-def update():
-    url = 'https://www.is-it-friday.org/'
-    friday = False
-    if (r := requests.get(url)):
-        friday = "YES!!! IT'S FRIDAY" in r.text
-
-    with open('.is_it_friday_data', 'w') as f:
-        print(friday, file=f)
-
-def get_text():
-    try:
-        with open('.is_it_friday_data') as f:
-            cached_data = f.readline().strip()
-            friday = cached_data == 'True'
-
-        return f'Today is {"not "*(not friday)}Friday.'
-
-    except FileNotFoundError:
-        return 'Unknown if today is Friday.'
